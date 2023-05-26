fb5e8fb8548cc6253efc9a842d2f234b2e0d4b7a
William Simionatto <william.sn0226@gmail.com>
2023-05-26 20:48:31 -0300

    feat: Draw the index finger path as per the saved coordinates

--- root.py
+++ root.py
@@ -5,6 +5,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
+path = []
 
 with mp_hands.Hands(
     static_image_mode=True,
@@ -15,8 +16,8 @@ with mp_hands.Hands(
     ret, frame = cap.read()
 
     if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+      print("Can't receive frame (stream end?). Exiting ...")
+      break
 
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -24,7 +25,6 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     cv2.putText(image, "Feche a mao para encerrar!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(
@@ -43,10 +43,24 @@ with mp_hands.Hands(
 
         if thumb_tip.y < index_tip.y < middle_tip.y < ring_tip.y < pinky_tip.y: # Verificar se a mão está fechada
           cv2.putText(image, "Mao fechada!", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+          cap.release()
+          cv2.destroyAllWindows()
+          break
+
         elif index_tip.y < middle_tip.y and index_tip.y < ring_tip.y and index_tip.y < pinky_tip.y: # Verificar se o dedo indicador está erguido
-          cv2.putText(image, "Dedo indicador erguido!", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+          cv2.putText(image, "Dedo indicador erguido! Para limpar abra a mao", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+
+          # Salva as coordenadas do dedo indicador
+          index_tip_x = int(index_tip.x * image.shape[1])
+          index_tip_y = int(index_tip.y * image.shape[0])
+          path.append((index_tip_x, index_tip_y))
+
+          for i in range(1, len(path)): # Desenhar o caminho do dedo indicador conforme as coordenadas salvas
+            cv2.line(image, path[i-1], path[i], (255, 255, 0), 2)
         else:
+          path = []
           cv2.putText(image, "Mao aberta!", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
 
     cv2.imshow('Hand Tracking', image)
     if cv2.waitKey(10) & 0xFF == ord('q'):
