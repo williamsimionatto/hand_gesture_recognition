bf2c2697c3e8510afe41afed7c3916c45bf578f6
William Simionatto <william.sn0226@gmail.com>
2023-06-02 19:33:09 -0300

    feat: create tkinter_mode

--- hand_tracking.py
+++ hand_tracking.py
@@ -8,6 +8,7 @@ mp_hands = mp.solutions.hands
 cap = cv2.VideoCapture(0)
 path = []
 mode = 0
+tkinter_mode = 0
 
 with mp_hands.Hands(
     static_image_mode=True,
@@ -61,25 +62,26 @@ with mp_hands.Hands(
           for i in range(1, len(path)): # Desenhar o caminho do dedo indicador conforme as coordenadas salvas
             cv2.line(image, path[i-1], path[i], (255, 255, 0), 2)
         elif mode == 2:
-          cv2.putText(image, "Tkinter Mode", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-          thumbFinger = (int(thumb_tip.x * image.shape[1]), int(thumb_tip.y * image.shape[0]))
-          indexFinger = (int(index_tip.x * image.shape[1]), int(index_tip.y * image.shape[0]))
+          if tkinter_mode == 0:
+            cv2.putText(image, "Tkinter Mode", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            thumbFinger = (int(thumb_tip.x * image.shape[1]), int(thumb_tip.y * image.shape[0]))
+            indexFinger = (int(index_tip.x * image.shape[1]), int(index_tip.y * image.shape[0]))
 
-          cv2.circle(image, thumbFinger, 10, (255, 0, 0), -1)  # Adicionar círculo no dedo polegar
-          cv2.circle(image, indexFinger, 10, (255, 0, 0), -1)  # Adicionar círculo no dedo indicador
-          cv2.line(image, thumbFinger, indexFinger, (255, 0, 0), 2) # Adicionar uma linha ligando o dedo polegar e o dedo indicador
+            cv2.circle(image, thumbFinger, 10, (255, 0, 0), -1)  # Adicionar círculo no dedo polegar
+            cv2.circle(image, indexFinger, 10, (255, 0, 0), -1)  # Adicionar círculo no dedo indicador
+            cv2.line(image, thumbFinger, indexFinger, (255, 0, 0), 2) # Adicionar uma linha ligando o dedo polegar e o dedo indicador
 
-          # Calcular a distância entre os dedos polegar e indicador
-          distance = math.sqrt((thumb_tip.x - index_tip.x)**2 + (thumb_tip.y - index_tip.y)**2)
-          distance_normalized = min(distance / 0.5, 1.0)  # Normalizar a distância entre 0 e 1
+            # Calcular a distância entre os dedos polegar e indicador
+            distance = math.sqrt((thumb_tip.x - index_tip.x)**2 + (thumb_tip.y - index_tip.y)**2)
+            distance_normalized = min(distance / 0.5, 1.0)  # Normalizar a distância entre 0 e 1
 
-          # Gerar valor para controlar o slider Valor entre 0 e 100
-          slider_value = int(distance_normalized * 100)
-          cv2.putText(image, f"Slider: {slider_value}", (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-        else:
-          path = []
-          closed_hands = 0
-          cv2.putText(image, "Mao aberta!", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            # Gerar valor para controlar o slider Valor entre 0 e 100
+            slider_value = int(distance_normalized * 100)
+            cv2.putText(image, f"Slider: {slider_value}", (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+          else:
+            path = []
+            closed_hands = 0
+            cv2.putText(image, "Mao aberta!", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
     if closed_hands == 2:  # Verifica se todas as mãos estão fechadas
       cv2.putText(image, "Todas as mãos estão fechadas! Fechando o programa...", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
@@ -91,7 +93,7 @@ with mp_hands.Hands(
       cv2.putText(image, "Drawing mode enabled", (image.shape[1] - 260, image.shape[0] - 700), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
     cv2.putText(image, "Press D to enable a drawing", (image.shape[1] - 360, image.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-    cv2.putText(image, "Press E to disable a drawing", (image.shape[1] - 360, image.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+    cv2.putText(image, "Press E to reset modes", (image.shape[1] - 360, image.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
     cv2.imshow('Hand Tracking', image)
 
@@ -105,6 +107,12 @@ with mp_hands.Hands(
       mode = 0
     elif key == ord('t'):
       mode = 2
+    elif key == ord('c'):
+      mode = 2
+      tkinter_mode = 0
+    elif key == ord('z'):
+      mode = 2
+      tkinter_mode = 1
 
 cap.release()
 cv2.destroyAllWindows()
