26211a99f42268a9ea7a9aa2f1126cb6c5683555
Natan O Pelizzoni <natan2624@gmail.com>
2023-06-16 22:51:48 -0300

    refactor: Changed variable and functions names

--- hand_tracking_game.py
+++ hand_tracking_game.py
@@ -2,18 +2,18 @@ import cv2
 import mediapipe as mp
 import random
 
-def mostrar_texto(imagem, texto, posicao, tamanho_fonte=1):
-  cv2.putText(imagem, texto, posicao, cv2.FONT_HERSHEY_SIMPLEX, tamanho_fonte, (255, 0, 0), 2, cv2.LINE_AA)
+def show_text(frame, text, position, font_size=1):
+  cv2.putText(frame, text, position, cv2.FONT_HERSHEY_SIMPLEX, font_size, (255, 0, 0), 2, cv2.LINE_AA)
 
 
-def verificar_vencedor(player_gesto, computer_gesto):
-  if player_gesto == computer_gesto:
+def check_winner(player_gesture, computer_gesture):
+  if player_gesture == computer_gesture:
     return "Tie"
 
   if (
-    (player_gesto == "rock" and computer_gesto == "scissors") or 
-    (player_gesto == "paper" and computer_gesto == "rock") or 
-    (player_gesto == "scissors" and computer_gesto == "paper")
+    (player_gesture == "rock" and computer_gesture == "scissors") or 
+    (player_gesture == "paper" and computer_gesture == "rock") or 
+    (player_gesture == "scissors" and computer_gesture == "paper")
   ):
     return "Winner"
 
@@ -27,11 +27,11 @@ cap = cv2.VideoCapture(0)
 player_score = 0
 computer_score = 0
 game_started = False
-player_gesto_last = None
-player_gesto = None
-gestos = ["rock", "paper", "scissors"]
-computer_gesto = ""
-resultado = "Tie"
+player_gesture_last = None
+player_gesture = None
+gestures = ["rock", "paper", "scissors"]
+computer_gesture = ""
+result = "Tie"
 
 with mp_hands.Hands(
   min_detection_confidence=0.5,
@@ -61,47 +61,47 @@ with mp_hands.Hands(
           and thumb_tip > ring_finger_tip
           and thumb_tip > pinky_tip
         ):
-          player_gesto_last = player_gesto
-          player_gesto = "paper"
+          player_gesture_last = player_gesture
+          player_gesture = "Paper"
         elif (
           thumb_tip < index_finger_tip
           and thumb_tip < middle_finger_tip
           and thumb_tip < ring_finger_tip
           and thumb_tip < pinky_tip
         ):
-          player_gesto_last = player_gesto
-          player_gesto = "rock"
+          player_gesture_last = player_gesture
+          player_gesture = "Rock"
         elif (
           thumb_tip < thumb_ip and middle_finger_tip < middle_finger_dip
         ):
-          player_gesto_last = player_gesto
-          player_gesto = "scissors"
+          player_gesture_last = player_gesture
+          player_gesture = "Scissors"
         else:
-          player_gesto = None
+          player_gesture = None
 
-        if (player_gesto and game_started):
-          if (player_gesto_last != player_gesto):
-            computer_gesto = random.choice(gestos)
-            resultado = verificar_vencedor(player_gesto, computer_gesto)
+        if (player_gesture and game_started):
+          if (player_gesture_last != player_gesture):
+            computer_gesture = random.choice(gestures)
+            result = check_winner(player_gesture, computer_gesture)
 
-            if resultado == "Winner":
+            if result == "Winner":
               player_score += 1
-            elif resultado == "Loser":
+            elif result == "Loser":
               computer_score += 1
 
-          mostrar_texto(frame, "Computer: {}".format(computer_gesto), (frame.shape[1] - 250, 80), tamanho_fonte=0.7)
-          mostrar_texto(frame, "Player: {}".format(player_gesto), (frame.shape[1] - 250, 40), tamanho_fonte=0.7)
-          mostrar_texto(frame, resultado, (int(frame.shape[1] / 2) - 50, int(frame.shape[0] / 2)))
+          show_text(frame, "Computer: {}".format(computer_gesture), (frame.shape[1] - 250, 80), font_size=0.7)
+          show_text(frame, "Player: {}".format(player_gesture), (frame.shape[1] - 250, 40), font_size=0.7)
+          show_text(frame, result, (int(frame.shape[1] / 2) - 50, int(frame.shape[0] / 2)))
 
     if game_started:
-      mostrar_texto(frame, "Player = {}".format(player_score), (20, 40))
-      mostrar_texto(frame, "Computer = {}".format(computer_score), (20, 80))
-      mostrar_texto(frame, "Press Q to finish game", (20, frame.shape[0] - 20))
+      show_text(frame, "Player = {}".format(player_score), (20, 40))
+      show_text(frame, "Computer = {}".format(computer_score), (20, 80))
+      show_text(frame, "Press Q to finish game", (20, frame.shape[0] - 20))
     else:
-      mostrar_texto(frame, "Press S to start game", (20, frame.shape[0] - 20))
+      show_text(frame, "Press S to start game", (20, frame.shape[0] - 20))
 
     key = cv2.waitKey(1)
-    cv2.imshow("Gesture Recognition", frame)
+    cv2.imshow("Rock Paper Scissor Game", frame)
 
     if key == ord("s") and not game_started:
       game_started = True
@@ -109,15 +109,15 @@ with mp_hands.Hands(
       computer_score = 0
     
     if key == ord("q"):
-      resultado_final = "Tie"
+      result_final = "Tie"
       if player_score > computer_score:
-        resultado_final = "Victory"
+        result_final = "Victory"
       elif player_score < computer_score:
-        resultado_final = "Defeat"
+        result_final = "Defeat"
 
-      print(resultado_final)
-      mostrar_texto(frame, resultado_final, (int(frame.shape[1] / 2) - 50, int(frame.shape[0] / 2)))
-      cv2.imshow("Gesture Recognition", frame)
+      print(result_final)
+      show_text(frame, result_final, (int(frame.shape[1] / 2) - 50, int(frame.shape[0] / 2)))
+      cv2.imshow("Rock Paper Scissor Game", frame)
       cv2.waitKey(2000)
       break
 
